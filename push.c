3c75046c2ced0cd9e8bb358c1864558b05271e00
ochuks1 <cochuks@gmail.com>
2024-02-08 09:25:03 -0800

    Task 0

--- push.c
+++ push.c
@@ -1,10 +1,10 @@
-nclude "monty.h"
+#include "monty.h"
 /**
- *  * f_push - add node to the stack
- *   * @head: stack head
- *    * @counter: line_number
- *     * Return: no return
- *     */
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
 void f_push(stack_t **head, unsigned int counter)
 {
 	int n, j = 0, flag = 0;
