3217c1dd627fb5e07a1a348f48db90e35deedada
ochuks1 <cochuks@gmail.com>
2024-02-21 08:42:21 -0800

    TASK

--- errors_2.c
+++ errors_2.c
@@ -9,15 +9,15 @@ int pchar_error(unsigned int line_number, char *message);
 
 
 /**
- *  * pop_error - Prints pop error messages for empty stacks.
- *   * @line_number: Line number in script where error occured.
- *    *
- *     * Return: (EXIT_FAILURE) always.
- *      */
+ * pop_error - Prints pop error messages for empty stacks.
+ * @line_number: Line number in script where error occured.
+ *
+ * Return: (EXIT_FAILURE) always.
+ */
 int pop_error(unsigned int line_number)
 {
-	    fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-	        return (EXIT_FAILURE);
+fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+return (EXIT_FAILURE);
 }
 
 
@@ -29,8 +29,8 @@ int pop_error(unsigned int line_number)
  */
 int pint_error(unsigned int line_number)
 {
-	    fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-	        return (EXIT_FAILURE);
+fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+return (EXIT_FAILURE);
 }
 
 
@@ -44,8 +44,8 @@ int pint_error(unsigned int line_number)
  */
 int short_stack_error(unsigned int line_number, char *op)
 {
-	    fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
-	        return (EXIT_FAILURE);
+fprintf(stderr, "L%u: can't %s, stack too short\n", line_number, op);
+return (EXIT_FAILURE);
 }
 
 
@@ -57,8 +57,8 @@ int short_stack_error(unsigned int line_number, char *op)
  */
 int div_error(unsigned int line_number)
 {
-	    fprintf(stderr, "L%u: division by zero\n", line_number);
-	        return (EXIT_FAILURE);
+fprintf(stderr, "L%u: division by zero\n", line_number);
+return (EXIT_FAILURE);
 }
 
 
@@ -72,7 +72,7 @@ int div_error(unsigned int line_number)
  */
 int pchar_error(unsigned int line_number, char *message)
 {
-	    fprintf(stderr, "L%u: can't pchar, %s\n", line_number, message);
-	        return (EXIT_FAILURE);
+fprintf(stderr, "L%u: can't pchar, %s\n", line_number, message);
+return (EXIT_FAILURE);
 }
 
